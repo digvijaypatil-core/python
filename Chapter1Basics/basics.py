ef6e6023fc38173b78466a9ea4489579728494ed
Digvijay <workfordvp@gmail.com>
2026-01-10 16:40:06 +0530

    basics of py

---
+++ Chapter1Basics/basics.py
@@ -0,0 +1,34 @@
+print("hello digvijay ")
+
+#lets go through integers in python
+a=5
+print(a)
+
+b=938973937539573957373953595739
+print(b)
+
+#floating point pi which we use in many calculations 
+pi=3.14
+print(pi)
+
+
+#String usage
+c='A'
+print(c)
+
+name='digvijay patil'
+print(name)
+
+#boolean varibale t/f
+
+q=True
+print(q)
+
+#empty value or say null data type we use none
+x=None
+print(x)
+
+#variable assignment works in py from left to right so you cant do something like 0=x or 5=x , it has to be x=0, x=5
+
+#you ll get syntax error if you do so
+
